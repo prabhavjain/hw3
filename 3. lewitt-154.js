fccf8c5d498bc827e1e632cbae5e49a6f2069a9f
prabhavjain <35622237+prabhavjain@users.noreply.github.com>
2018-02-05 21:14:16 -0800

    Create 3. lewitt-154.js

---
+++ 3. lewitt-154.js
@@ -0,0 +1,7 @@
+//Link to drawing - http://massmoca.org/event/walldrawing154/
+
+a = width/7
+noFill();
+rect ( a, a, height - a*2 , height - a*2 )
+stroke("red")
+line ( a, height/2, a*4, height/2)

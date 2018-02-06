f42e5c0c360dd949680d7a8af5072ea9120d9b96
prabhavjain <35622237+prabhavjain@users.noreply.github.com>
2018-02-05 21:08:45 -0800

    Create 3. lewitt-160.js

---
+++ 3. lewitt-160.js
@@ -0,0 +1,11 @@
+//link to drawing: http://massmoca.org/event/walldrawing160/
+
+a = width/7
+
+line (a, a, a * 6, a * 6)
+line (a, a * 6,  a * 6, a)
+
+z = a/4
+
+noFill();
+rect ( z, z, height- z*2 , height- z*2 )
